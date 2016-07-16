3cbe642b320cd3443ed45dce206f1dbef152059d
Jef <Jef.statham@gmail.com>
2016-07-16 00:13:59 -0400

    v1.1 PredictionMarket contract add timegates remove for loop

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -22,9 +22,10 @@ contract Administrated {
     event AdminRemoved(address removedAdmin);
     
     mapping(address => bool) isAdmin;
+    uint adminCount;
     
     function Administrated() {
-        isAdmin[msg.sender] = true;
+        addAdmin(msg.sender);
     }
     
     modifier onlyAdmins() {
@@ -32,14 +33,22 @@ contract Administrated {
         _
     }
     
-    function addAdmin(address newAdmin) onlyAdmins {
+    function addAdmin(address newAdmin) onlyAdmins returns(bool success) {
+        if (msg.value > 0) throw;
+        if (isAdmin[newAdmin]) return false;
         isAdmin[newAdmin] = true;
+        adminCount++;
         AdminAdded(newAdmin);
+        return true;
     }
     
-    function removeAdmin(address curAdmin) onlyAdmins {
+    function removeAdmin(address curAdmin) onlyAdmins returns(bool success) {
+        if (msg.value > 0) throw;
+        if (!isAdmin[curAdmin] || adminCount - 1 == 0) return false;
         isAdmin[curAdmin] = false;
+        adminCount--;
         AdminRemoved(curAdmin);
+        return true;
     }
 }
 
@@ -48,11 +57,14 @@ contract PredictionMarket is Owned, Administrated {
     event PollClosed();
     event PollOpened();
     event PredictionUpdate(uint totalYes, uint totalNo);
-    event PaidOut();
+    event PaidOut(address recipient);
+
+    uint constant payoutCollectionWindow = 1 days;
 
     struct Bet {
         uint yes;
         uint no;
+        bool paidOut;
     }
     
     struct YesNoQuestion {
@@ -61,11 +73,10 @@ contract PredictionMarket is Owned, Administrated {
         uint totalNo;
         bool open;
         bool result; //true == yes, false == no
-        bool paidOut;
+        uint closingDate;
+        uint closingBalance;
         address trustedSource;
         mapping(address => Bet) bets;
-        mapping(uint => address) gamblers;
-        uint gamblerCount;
     }
 
     YesNoQuestion poll;
@@ -73,16 +84,20 @@ contract PredictionMarket is Owned, Administrated {
     function PredictionMarket() Owned() Administrated() {
     }
     
-    function createNewPoll(string question, address trustedSource) private {
+    function createNewPoll(
+    string question
+    ,address trustedSource
+    ,uint closingDate
+    ) private {
         poll = YesNoQuestion({
             question:question,
             totalYes: 0,
             totalNo: 0,
             open: true,
             result: false,
-            paidOut: false,
-            trustedSource: trustedSource,
-            gamblerCount: 0
+            closingDate: closingDate,
+            closingBalance: 0,
+            trustedSource: trustedSource
         });
     }
     
@@ -96,66 +111,70 @@ contract PredictionMarket is Owned, Administrated {
         _
     }
     
-    function setNewTrustedSource(address trustedSource) onlyAdmins openPoll returns (bool success) {
+    modifier noMsgValue(){
+        if (msg.value > 0) throw;
+        _
+    }
+    
+    function setNewTrustedSource(address trustedSource) onlyAdmins openPoll noMsgValue returns (bool success) {
         poll.trustedSource = trustedSource;
         return true;
     }
     
-    function openNewPoll(string question, address trustedSource) onlyAdmins closedPoll returns (bool success) {
-        if (!poll.paidOut) throw;
-        createNewPoll(question, trustedSource);
+    function openNewPoll(string question, address trustedSource, uint closingDate) onlyAdmins closedPoll noMsgValue returns (bool success) {
+        if (poll.closingDate + payoutCollectionWindow > now ) throw;
+        createNewPoll(question, trustedSource, closingDate);
+        // cloud xfer any contract balance to the owner here,
+        // instead let it roll into the next poll payout.
+        // Otherwise an enterprising admin could start a poll
+        // with themselves as the trustedsource close it then 
+        // collect the contracts outstanding balance.
         PollOpened();
         return true;
     }
     
-    function closePoll(bool result) openPoll returns (bool success) {
-        if (poll.trustedSource != msg.sender) throw;
-        poll.result = result;
-        poll.open = false;
-        PollClosed();
-        return true;
+    function closePoll(bool result) openPoll noMsgValue returns (bool success) {
+        if (poll.trustedSource == msg.sender
+         || poll.closingDate < now) {
+            poll.result = result;
+            poll.open = false;
+            poll.closingDate = now;
+            poll.closingBalance = this.balance;
+            PollClosed();
+            return true;
+        }
+        throw;
     }
     
-    function payOut() closedPoll returns (bool success) {
-        if (poll.paidOut) throw;
-        poll.paidOut = true;
-        var initialContractBalance = this.balance;
+    function payOut() closedPoll noMsgValue returns (bool success) {
+        if (poll.bets[recipient].paidOut) throw;
+        poll.bets[recipient].paidOut = true;
         uint totalBets = poll.totalYes + poll.totalNo;
-        for(uint i=0;i<poll.gamblerCount;i++)
-        {
-            address recipient = poll.gamblers[i];
-            Bet bet = poll.bets[recipient];
-            uint percentageOfPot = poll.result
-                ? bet.yes / totalBets
-                : bet.no / totalBets;
-            if (percentageOfPot > 1) throw;
-            // This is how the mapping is cleaned up in the poll struct
-            bet.yes = 0;
-            bet.no = 0;
-            var payout = initialContractBalance * percentageOfPot;
-            // Handle divsion and multiplication rounding errors.
-            // Sucks a little bit to be the last bet
-            if (payout > this.balance) {
-                payout = this.balance;
-            }
-            // TODO maybe an event for sending
-            if (!recipient.send(payout)) throw;
+
+        address recipient = msg.sender;
+        Bet bet = poll.bets[recipient];
+        uint percentageOfPot = poll.result
+            ? bet.yes / totalBets
+            : bet.no / totalBets;
+        if (percentageOfPot > 1) throw;
+        // This is how the mapping is cleaned up in the poll struct
+        bet.yes = 0;
+        bet.no = 0;
+        var payout = poll.closingBalance * percentageOfPot;
+        // Handle divsion and multiplication rounding errors.
+        // Sucks a little bit to be the last person to collect
+        if (payout > this.balance) {
+            payout = this.balance;
         }
-        PaidOut();
+        
+        if (!recipient.send(payout)) throw;
+
+        PaidOut(recipient);
         return true;
     }
     
-    modifier maybeAddGambler() {
+    function betFor() openPoll returns (bool success) {
         if (msg.value == 0) throw; //no freebies!
-        var sender = msg.sender;
-        if (   poll.bets[sender].yes == 0
-            || poll.bets[sender].no  == 0 ) {
-            poll.gamblers[poll.gamblerCount++] = sender;
-        }
-        _
-    }
-    
-    function betFor() openPoll maybeAddGambler returns (bool success) {
         var value = msg.value;
         poll.totalYes += value;
         poll.bets[msg.sender].yes += value;
@@ -163,7 +182,8 @@ contract PredictionMarket is Owned, Administrated {
         return true;
     }
     
-    function betAgainst() openPoll maybeAddGambler returns (bool success) {
+    function betAgainst() openPoll returns (bool success) {
+        if (msg.value == 0) throw; //no freebies!
         var value = msg.value;
         poll.totalNo += value;
         poll.bets[msg.sender].no += value;
@@ -176,7 +196,7 @@ contract PredictionMarket is Owned, Administrated {
         throw;
     }
 
-    function kill() onlyOwner returns (bool success) { 
+    function kill() onlyOwner closedPoll returns (bool success) { 
         selfdestruct(owner); //should actually refund bets
         return true;
     } 
